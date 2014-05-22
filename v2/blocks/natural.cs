ddb56a9c13bfccd96856905ad71305057e28d07c
ottosparks <ottosparks@hotmail.com>
2014-05-22 15:21:38 -0400

    v2
    fixed falling blocks & cave in; disabled cave in by default (needs heavy
    optimisation); improved block update system; implemented more
    local-scoped box search; several general optimisations

---
+++ v2/blocks/natural.cs
@@ -0,0 +1,70 @@
+if(!isFunction(blastOff_NewPreload))
+	exec("./blastoff.cs");
+if(!isObject(psVoxBlockGroup))
+	psVoxBlockGroup_New();
+
+if(!isObject(psVoxBlockData_Dirt))
+{
+	new ScriptObject(psVoxBlockData_Dirt)
+	{
+		class = "psVoxBlockData_Natural";
+		superClass = "psVoxBlockData";
+		name = "Dirt";
+
+		shapeType = "Block";
+		shape = blastOff_NewPreload("BDDirt", "STRING", "4x Cube\" 0 0 0 0 1" SPC getClosestPaintColor("0.53 0.3 0 1") SPC "0 0 0 1 1 1");
+		solid = true;
+		opaque = true;
+		trigger = false;
+		touch = false;
+		posFromBottom = false;
+		rotates = false;
+		activated = false;
+		carryProps = false;
+		gravity = false;
+		stable = false;
+
+		breakSpeed = 5;
+		weight = 1;
+		support = -1;
+	};
+}
+
+if(!isObject(psVoxBlockData_Stone))
+{
+	new ScriptObject(psVoxBlockData_Stone : psVoxBlockData_Dirt)
+	{
+		name = "Stone";
+
+		shape = blastOff_NewPreload("BDStone", "STRING", "4x Cube\" 0 0 0 0 1" SPC getClosestPaintColor("0.75 0.75 0.75 1") SPC "0 0 0 1 1 1");
+
+		breakSpeed = 10;
+	};
+}
+
+if(!isObject(psVoxBlockData_HardStone))
+{
+	new ScriptObject(psVoxBlockData_HardStone : psVoxBlockData_Dirt)
+	{
+		name = "Hard Stone";
+
+		shape = blastOff_NewPreload("BDHardStone", "STRING", "4x Cube\" 0 0 0 0 1" SPC getClosestPaintColor("0.3 0.3 0.3 1") SPC "0 0 0 1 1 1");
+		stable = true;
+
+		breakSpeed = 25;
+		weight = 5;
+	};
+}
+
+if(!isObject(psVoxBlockData_Sand))
+{
+	new ScriptObject(psVoxBlockData_Sand : psVoxBlockData_Dirt)
+	{
+		name = "Sand";
+
+		shape = blastOff_NewPreload("BDSand", "STRING", "4x Cube\" 0 0 0 0 1" SPC getClosestPaintColor("1 0.94 0.7 1") SPC "0 0 0 1 1 1");
+		gravity = true;
+
+		breakSpeed = 5;
+	};
+}
---
+++ v2/mech/update.cs
@@ -0,0 +1,85 @@
+$PsVox::UpdateSpeed = 250;
+$PsVox::UpdateSize = 100;
+
+function psVoxUpdater_New()
+{
+	if(isObject(psVoxUpdater))
+		return psVoxUpdater;
+
+	new SimSet(psVoxUpdater)
+	{
+		psVoxUpdater = true;
+	};
+	psVoxUpdater.psVoxUpdate();
+	if(isObject(PsVox))
+		PsVox.updater = psVoxUpdater;
+
+	return psVoxUpdater;
+}
+
+function psVoxBlock::onUpdate(%this, %obj)
+{
+	//pass
+}
+
+function psVoxBlock::enqUpdate(%this)
+{
+	if(!isObject(psVoxUpdater))
+	{
+		%this.type.onUpdate(%this);
+		return;
+	}
+	psVoxUpdater.psVoxEnqueue(%this);
+}
+
+function psVoxBlock::update(%this)
+{
+	// %this.type.schedule($PsVox::UpdateTick, onUpdate, %this, %this);
+	// %this.type.onUpdate(%this, %this);
+	%this.enqUpdate(%this);
+	%this.psVox.startBoxSearch(%this.pos, $PsVox::UpdateReach);
+	while((%obj = %this.psVox.boxSearchNext()) != 0)
+	{
+		if(%obj == -1)
+			continue;
+
+		// %obj.type.shedule(1$PsVox::UpdateTick, onUpdate, %obj, %this);
+		%obj.enqUpdate();
+	}
+}
+
+function SimSet::psVoxEnqueue(%this, %obj)
+{
+	%this.add(%obj);
+	%this.bringToFront(%obj);
+}
+
+function SimSet::psVoxUpdate(%this)
+{
+	cancel(%this.updateTick);
+	if(!%this.psVoxUpdater)
+		return;
+
+	%ct = %this.getCount();
+	if(%ct > 0)
+	{
+		%ct = %ct - 1;
+		if(%ct < $PsVox::UpdateSize)
+			%end = 0;
+		else
+			%end = %ct - $PsVox::UpdateSize;
+		for(%i = %ct; %i >= %end; %i--)
+		{
+			%obj = %this.getObject(%i);
+			if(!isObject(%obj))
+			{
+				%this.remove(%obj);
+				continue;
+			}
+			%obj.type.onUpdate(%obj);
+			%this.remove(%obj);
+		}
+	}
+
+	$PsVox::UpdateTick = %this.updateTick = %this.schedule($PsVox::UpdateSpeed, psVoxUpdate);
+}
